142568827f7ff4681d40604138a09296a82f03f4
dearjh36 <dearjh36@naver.com>
2024-08-29 03:22:33 +0900

    8/29

---
+++ code_test/src/backjoon/aug/No1436.java
@@ -0,0 +1,14 @@
+package backjoon.aug;
+
+import java.util.Scanner;
+
+public class No1436 {
+    public static void main(String[] args) throws Exception {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        
+    }
+
+}
