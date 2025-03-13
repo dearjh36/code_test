5b1a3f98fd53633d7562e1c6855fd3f33df12d4f
dearjh36 <dearjh36@naver.com>
2025-03-13 20:07:03 +0900

    3/13
    카운팅 정렬과 선택 정렬
    알고리즘 정렬은 따로 프로젝트 만들어서 정렬 알고리즘은 여기까지

--- code_test/src/backjoon/Mar/Counting_Sort.java
+++ code_test/src/backjoon/Mar/Counting_Sort.java
@@ -5,7 +5,7 @@ public class Counting_Sort {
     public static void main(String[] args){
 
         int[] arr = new int[100];        // 수열의 원소 : 100개
-        int[] cnt = new int[30];        // 수의 범위 : 0 ~ 30
+        int[] cnt = new int[31];        // 수의 범위 : 0 ~ 30
         int[] result = new int[100];    // 정렬될 배열
 
 
@@ -17,26 +17,29 @@ public class Counting_Sort {
         // Counting Sort
         // 과정 1
         for (int i = 0; i < arr.length; i++){
-            // arr 의 value 값을 index로 하는 cnt 배열 값 1 증가
-            // value 가 1 이면 cnt[1] 이 1++
-            cnt[arr[i]]++;
+            cnt[arr[i]] ++;
         }
 
         // 과정 2
-        for(int i = 1; i < cnt.length; i++) {
-            // 누적 합 해주기
-            cnt[i] += cnt[i - 1];
+        for (int i = 1; i < cnt.length ; i++){
+            cnt[i] += cnt[i-1];
         }
 
         // 과정 3
-        for(int i = arr.length - 1; i >= 0; i--) {
-            /*
-             *  i 번쨰 원소를 인덱스로 하는 counting 배열을 1 감소시킨 뒤
-             *  counting 배열의 값을 인덱스로 하여 result에 value 값을 저장한다.
-             */
+        //
+        for (int i = arr.length - 1; i >=0; i--){
+
             int value = arr[i];
             cnt[value]--;
             result[cnt[value]] = value;
+
+            // 한 줄로 만들면 이렇긴한데 보기 좋진 않은듯..
+            // result[--cnt[arr[i]]] = arr[i];
+
+        }
+
+        for (int i:result){
+            System.out.print(result[i] + " ");
         }
 
     }
--- code_test/src/backjoon/oct/Selection_Sort.java
+++ code_test/src/backjoon/oct/Selection_Sort.java
@@ -16,4 +16,27 @@ public class Selection_Sort {
 
         
     }
+
+    private static void selecton_sort(int[] a, int size){
+        for(int i = 0; i < size; i++){
+            int min_index = i;
+
+            // 최솟값 인덱스 찾기
+            for (int j = i + 1; j < size; j++){ // i +1 이후
+                if (a[j] < a[min_index]){
+                    min_index = j;
+                }
+            }
+
+            swap(a, min_index, i);
+        }
+    }
+
+    // 배열까지 받아야 바뀐게 배열에 적용
+    // 따로 swap() 을 두는 이유는 코드 재사용성 증가, 가독성 향ㅅ아, 디버깅 편리 를 위해서다.
+    private static  void  swap(int[] a, int i, int j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 }
